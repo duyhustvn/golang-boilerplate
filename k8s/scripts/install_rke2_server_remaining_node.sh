efb8ff46cedd2233c9d3a7d4e3c23c90f6bd1f26
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2024-07-20 00:14:21 +0700

    update vms

--- k8s/scripts/install_rke2_server_remaining_node.sh
+++ k8s/scripts/install_rke2_server_remaining_node.sh
@@ -41,42 +41,19 @@ EOF
 "
     vagrant ssh $vmname -c 'sudo mv config.yaml /etc/rancher/rke2'
 
-done
+    echo "Enable rke2-server"
+    vagrant ssh $vmname -c 'sudo systemctl enable rke2-server'
+    echo "Start rke2-server"
+    vagrant ssh $vmname -c 'sudo systemctl start rke2-server'
 
-#for ((i = 2; i <= $NUMBER_OF_VMS; i++)); do
-#  machine=vm$i
-#  vagrant ssh $machine -c 'sudo mkdir -p /etc/rancher/rke2'
-#
-#  node_name=node-master-0$i
-#
-#  vagrant ssh $machine -c "
-#cat <<EOF > ~/config.yaml
-#node-name: $node_name
-#token: $token
-#debug: true
-## disable: rke2-ingress-nginx
-#cni:
-#  - canal
-#disable-cloud-controller: true
-#enable-servicelb: true
-#kube-apiserver-arg:
-#  - 'default-not-ready-toleration-seconds=30'
-#  - 'default-unreachable-toleration-seconds=30'
-#EOF
-#"
-#  vagrant ssh $machine -c 'sudo mv config.yaml /etc/rancher/rke2'
-#
-#  echo "Enable rke2-server"
-#  vagrant ssh $machine -c 'sudo systemctl enable rke2-server'
-#
-#  echo "Start rke2-server"
-#  vagrant ssh $machine -c 'sudo systemctl start rke2-server'
-#
-#  kubectl_exists=$(vagrant ssh $machine -c "command -v kubectl")
-#  if [ -z $kubectl_exists ]; then
-#    echo "kubectl not installed. Install it"
-#    vagrant ssh $machine -c "sudo ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/"
-#  else
-#    echo "kubectl already installed. skip"
-#  fi
-#done
+    echo "Install kubectl"
+
+    kubectl_exists=$(vagrant ssh $vmname -c "command -v kubectl")
+    if [ -z $kubectl_exists ]; then
+    echo "kubectl not installed. Install it"
+    vagrant ssh $vmname -c "sudo ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/"
+    else
+      echo "kubectl already installed. skip"
+    fi
+
+done
