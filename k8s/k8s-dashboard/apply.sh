a0db2a93396cc9dc6bfc97536eed826e9b9605ae
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2024-11-09 22:33:39 +0700

    k8s dashboard

---
+++ k8s/k8s-dashboard/apply.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+helm repo add kubernetes-dashboard https://kubernetes.github.io/dashboard/
+
+helm upgrade --install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard --create-namespace --namespace kubernetes-dashboard
+
+kubectl apply -f dashboard-admin.yaml
