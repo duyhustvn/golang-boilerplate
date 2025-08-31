f6c6b6e38a647e9d36c2ce961c2a59afd6898450
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2025-09-01 01:17:17 +0700

    remove unused code

--- deployments/database/pgpool2/02.pre_process.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Stop and disable etcd, patroni, postgresql"
-sudo systemctl stop {etcd,patroni,postgresql,pgpool2,pgbouncer}
-sudo systemctl disable {etcd,patroni,postgresql,pgpool2,pgbouncer}
-
-echo "Delete default value of postgresql"
-sudo rm -rf /var/lib/postgresql/16/main
--- k8s/scripts/install_vagrant.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-echo "Download and setup gpg key"
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-
-echo "Install vagrant"
-sudo apt update && sudo apt install vagrant
