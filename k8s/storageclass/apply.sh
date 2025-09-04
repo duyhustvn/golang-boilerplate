6a37ed5aacc7f43df36547a4fa70b181fb725a51
duyhustvn <duy.dinhle.hust.vn@gmail.com>
2025-09-05 05:47:18 +0700

    deploy storageclass

--- k8s/storageclass/apply.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f storageclass.yaml
-# Set local-path-storage to default storageclass
-kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
